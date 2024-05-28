f0f82ee4b386937ddb1def4e134229600a1f4a6f
raef <spacegsus@icloud.com>
2024-05-28 19:26:17 +0330

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
