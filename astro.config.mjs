ae2b299c5c7d29dc7ba15fd252b72462c2ca7f87
Raef <raef@Raefs-Mac-mini.local>
2025-02-26 04:44:50 +0330

    Hero Style

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    splitting: true,
+    minify: true,
+  },
   integrations: [
     tailwind({
       config: { applyBaseStyles: false },
